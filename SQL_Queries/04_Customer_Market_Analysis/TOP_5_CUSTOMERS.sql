89ec8d747ff7ce3e67bae28b2a0df08164946d48
SUBHAYAN KUNDU <kundusubhayan@gmail.com>
2026-03-21 17:43:56 +0530

    Add files via upload

--- SQL_Queries/04_Customer_Market_Analysis/TOP_5_CUSTOMERS.sql
+++ SQL_Queries/04_Customer_Market_Analysis/TOP_5_CUSTOMERS.sql
@@ -1,1 +1,10 @@
-
+SELECT
+	customers ,
+	round(sum(net_sales)/1000000,2) as Net_Sales_millions
+	from net_sales
+	where fiscal_year=2021
+	group by customers
+	order by 
+	Net_Sales_millions
+	desc
+	limit 5
