371e6f816185ba15db4828401feba8c3570c1508
SUBHAYAN KUNDU <kundusubhayan@gmail.com>
2026-03-21 17:36:19 +0530

    Add files via upload

--- SQL_Queries/01_Data_Overview/Data_Overview.sql
+++ SQL_Queries/01_Data_Overview/Data_Overview.sql
@@ -1,1 +1,16 @@
+SELECT * FROM fact_sales_monthly;
+-- All availabe markets
+select DISTINCT market from dim_customer;
+-- All distribution methods
+select DISTINCT channel from dim_customer;
+-- All operating regions
+select DISTINCT region from dim_customer;
+-- product divisions in which AtliQ usually operates
+select DISTINCT division from dim_product;
+-- Every product segments in production
+select DISTINCT segment from dim_product;
+-- Business product categories
+select DISTINCT category from dim_product;
 
+-- division -> segment -> category -> product -> variant
+-- the above is the product hirarchy 
