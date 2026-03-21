4a1acf4bbbb92292871a8f938f4f4172ef9a1546
SUBHAYAN KUNDU <kundusubhayan@gmail.com>
2026-03-21 20:32:37 +0530

    Delete SQL_Queries/05_Product_Analysis/window_rank,des[rank].sql

--- SQL_Queries/05_Product_Analysis/window_rank,des[rank].sql
+++
@@ -1,8 +0,0 @@
-with cte1 as(SELECT * ,
-row_number() over(partition by category order by category desc) as rn,
-rank() over(partition by category order by category desc) as rnk,
-dense_rank() over(partition by category order by category desc) as d_rnk
-FROM expenses
-order by category)
-
-select * from cte1 where d_rnk<=2
